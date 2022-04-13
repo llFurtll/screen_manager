32964566813f5d7d7bc83bfaa092680ce95fc46f
Daniel Melonari <daniel_melonari@hotmail.com>
2022-04-13 20:14:07 -0300

    Created unit tests of the interfaces

---
+++ test/component/icomponent_test.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutterx/domain/interfaces/icomponent.dart';
+import 'package:flutterx/infra/implementations/any_screen.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'icomponent_test.mocks.dart';
+
+class FakeComponent extends Mock implements IComponent<AnyScreen, Container, int> {}
+
+@GenerateMocks([FakeComponent])
+void main() {
+  late FakeComponent fakeComponent;
+
+  setUp(() {
+    fakeComponent = MockFakeComponent();
+  });
+
+  group("Tests methods abstract class IComponent", () {
+    test("init", () {
+      expect(() => fakeComponent.init(), returnsNormally);
+      verify(fakeComponent.init()).called(1);
+    });
+
+    test("constructor", () {
+      when(fakeComponent.constructor()).thenReturn(Container());
+      expect(fakeComponent.constructor(), isInstanceOf());
+      verify(fakeComponent.constructor()).called(1);
+    });
+
+    test("beforeEvent", () {
+      when(fakeComponent.beforeEvent()).thenReturn(1);
+      expect(fakeComponent.beforeEvent(), 1);
+      verify(fakeComponent.beforeEvent()).called(1);
+    });
+
+    test("event", () {
+      when(fakeComponent.event()).thenReturn(2);
+      expect(fakeComponent.event(), 2);
+      verify(fakeComponent.event()).called(1);
+    });
+
+    test("afterEvent", () {
+      when(fakeComponent.afterEvent()).thenReturn(3);
+      expect(fakeComponent.afterEvent(), 3);
+      verify(fakeComponent.afterEvent()).called(1);
+    });
+  });
+}
---
+++ test/screen/iscreen_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutterx/domain/interfaces/iscreen.dart';
+import 'package:mockito/mockito.dart';
+
+import '../component/icomponent_test.dart';
+import '../component/icomponent_test.mocks.dart';
+
+class FakeScreen extends Mock implements IScreen {}
+
+void main() {
+  late FakeScreen fakeScreen;
+  late FakeComponent fakeComponent;
+
+  setUp(() {
+    fakeScreen = FakeScreen();
+    fakeComponent = MockFakeComponent();
+  });
+
+  group("Tests methods abstract class IScreen", () {
+    test("emitScreen", () {
+      expect(() => fakeScreen.emitScreen(fakeComponent), returnsNormally);
+    });
+
+    test("receive", () {
+      expect(() => fakeScreen.receive("test", 0), returnsNormally);
+    });
+  });
+}
