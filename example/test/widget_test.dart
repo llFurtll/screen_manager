4fdd04bfe8492be1e4fd9ff70bddda94f3e14d79
Daniel Melonari <daniel.melonari@sv-ntb183.local>
2022-04-14 12:37:08 -0300

    Remove tests widgets

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutterx_example/ui/homepage/homepage.dart';
-
 void main() {
 
   Widget constructorHomePage({Widget? child}) {
@@ -9,21 +6,4 @@ void main() {
       home: child,
     );
   }
-
-  testWidgets("HomePage has a count and FloatActionButtonComponent", (WidgetTester tester) async {
-    await tester.pumpWidget(constructorHomePage(child: const HomePage()));
-
-    final countValueNotifier = find.text('0');
-    final floatAction = find.widgetWithIcon(FloatingActionButton, Icons.add);
-    
-    expect(countValueNotifier, findsOneWidget);
-    expect(floatAction, findsOneWidget);
-  });
-
-  testWidgets("HomePage increment count value when clicked in floatingActionButton", (WidgetTester tester) async {
-    await tester.pumpWidget(constructorHomePage(child: const HomePage()));
-    await tester.tap(find.byType(FloatingActionButton));
-
-    expect(find.text('1'), findsOneWidget);
-  });
 }
