6adadaf0503ae050ec8255b16d3f5d8bf400ed45
Daniel Melonari <daniel.melonari@SV-NTB894.local>
2022-12-16 15:46:33 -0300

    Finalizado testes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,3 @@
-import 'package:compmanager/screen_controller.dart';
-import 'package:compmanager/screen_injection.dart';
-import 'package:compmanager/screen_view.dart';
 import 'package:flutter/material.dart';
 
 import 'pages/first_page/view/first_page_view.dart';
@@ -8,23 +5,10 @@ import 'pages/second_page/view/second_page_view.dart';
 
 void main() {
   runApp(MaterialApp(
-    // initialRoute: "/",
-    home: Teste(),
-    // routes: {
-    //   FirstPage.firsPageRoute: (context) => const FirstPage(),
-    //   SecondPage.secondPageRoute:(context) => const SecondPage()
-    // },
+    initialRoute: "/",
+    routes: {
+      FirstPage.firsPageRoute: (context) => const FirstPage(),
+      SecondPage.secondPageRoute:(context) => const SecondPage()
+    },
   ));
-}
-
-// ignore: must_be_immutable
-class Teste extends ScreenView<NoController, NoScreenInjection> {
-  Teste({super.key});
-
-  @override
-  Scaffold build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
-    );
-  }
 }
